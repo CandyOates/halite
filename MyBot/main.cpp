6919469a238df1f3980c33cb157c60f38931228e
Andrew Coates <agcoates3@gmail.com>
2018-01-07 16:56:31 -0500

    Initial Commit

---
+++ MyBot/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MyBot
+//
+//  Created by Andrew Coates on 1/7/18.
+//  Copyright © 2018 Andrew Coates. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
